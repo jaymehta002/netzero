0c0c4592e242178f1d493cc3e33fd1d249d9599b
jaymehta002 <jaymehta002@gmail.com>
2024-09-26 13:41:15 +0530

    lint

--- src/components/ui/svg-mask-effect.tsx
+++ src/components/ui/svg-mask-effect.tsx
@@ -17,8 +17,11 @@ export const MaskContainer = ({
   className?: string;
 }) => {
   const [isHovered, setIsHovered] = useState(false);
+  //eslint-disable-next-line
   const [mousePosition, setMousePosition] = useState<any>({ x: null, y: null });
+  //eslint-disable-next-line
   const containerRef = useRef<any>(null);
+  //eslint-disable-next-line
   const updateMousePosition = (e: any) => {
     const rect = containerRef.current.getBoundingClientRect();
     setMousePosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
@@ -35,18 +38,18 @@ export const MaskContainer = ({
       }
     };
   }, []);
-  let maskSize = isHovered ? revealSize : size;
+  const maskSize = isHovered ? revealSize : size;
 
   return (
     <motion.div
       ref={containerRef}
       className={cn("h-screen relative", className)}
       animate={{
-        backgroundColor: isHovered ? "var(--white)" : "var(--white)",
+        backgroundColor: isHovered ? "var(--slate-900)" : "var(--white)",
       }}
     >
       <motion.div
-        className="w-full h-full flex items-center justify-center text-6xl absolute bg-primary bg-grid-white/[0.2] text-white [mask-image:url(/mask.svg)] [mask-size:40px] [mask-repeat:no-repeat]"
+        className="w-full h-full flex items-center justify-center text-6xl absolute bg-black bg-grid-white/[0.2] text-white [mask-image:url(/mask.svg)] [mask-size:40px] [mask-repeat:no-repeat]"
         animate={{
           maskPosition: `${mousePosition.x - maskSize / 2}px ${
             mousePosition.y - maskSize / 2
@@ -57,7 +60,7 @@ export const MaskContainer = ({
           duration: 0,
         }}
       >
-        <div className="absolute inset-0 bg-primary h-full w-full z-0 opacity-50" />
+        <div className="absolute inset-0 bg-black h-full w-full z-0 opacity-50" />
         <div
           onMouseEnter={() => {
             setIsHovered(true);
